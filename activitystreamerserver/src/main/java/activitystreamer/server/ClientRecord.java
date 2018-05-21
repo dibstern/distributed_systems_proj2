d55c507931cce37b261f84fe4544cabbc204dd53
Unknown <dibstern@gmail.com>
2018-05-21 17:57:36 +1000

    Fill out Functionality (ClientRecord & ClientRegistry)

--- activitystreamerserver/src/main/java/activitystreamer/server/ClientRecord.java
+++ activitystreamerserver/src/main/java/activitystreamer/server/ClientRecord.java
@@ -2,42 +2,41 @@ package activitystreamer.server;
 
 import org.json.simple.JSONObject;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ClientRecord {
 
     private String username;
     private String secret;
-    private Integer next_token;
+    private Integer next_token_num;
 
     // Maps usernames to the next expected message token (the token of the next message that the user has not received)
-    private HashMap<String, Integer> expected_tokens;
+    private ConcurrentHashMap<String, Integer> expected_tokens;
 
     // Maps tokens to JSONObject messages
-    private HashMap<Integer, JSONObject> sent_messages;
+    private ConcurrentHashMap<Integer, JSONObject> sent_messages;
 
 
     public ClientRecord(String username, String secret) {
         this.username = username;
         this.secret = secret;
-        this.next_token = 1;
-        this.expected_tokens = new HashMap<String, Integer>();
-        this.sent_messages = new HashMap<Integer, JSONObject>();
+        this.next_token_num = 1;
+        this.expected_tokens = new ConcurrentHashMap<String, Integer>();
+        this.sent_messages = new ConcurrentHashMap<Integer, JSONObject>();
     }
 
-    public ClientRecord(String username, String secret, Integer next_expected_token,
-                        HashMap<String, Integer> expected_tokens, HashMap<Integer, JSONObject> sent_messages) {
-        this.username = username;
-        this.secret = secret;
-        this.next_token = next_expected_token;
-        this.expected_tokens = expected_tokens;
-        this.sent_messages = sent_messages;
+    public ClientRecord(JSONObject clientRecordJson) {
+        this.username = clientRecordJson.get("username").toString();
+        this.secret = clientRecordJson.get("secret").toString();
+        this.next_token_num = ((Long) clientRecordJson.get("next_token_num")).intValue();
+        this.expected_tokens = toTokenHashMap((JSONObject) clientRecordJson.get("expected_tokens"));
+        this.sent_messages = toSentMessagesHashMap((JSONObject) clientRecordJson.get("sent_messages"));
     }
 
-    public void updateMessagesSent(HashMap<Integer, JSONObject> sent_messages_record) {
+    public void updateMessagesSent(ConcurrentHashMap<Integer, JSONObject> sent_messages_record) {
         sent_messages_record.forEach((token, jsonMsg) -> {
-            if (!sent_messages.containsKey(token)) {
-                sent_messages.put(token, jsonMsg);
+            if (!this.sent_messages.containsKey(token)) {
+                this.sent_messages.put(token, jsonMsg);
             }
             // TODO: Either here, or write another function called immediately after this, that sends messages to other
             // clients if they are expecting this new token. Maybe a function in ClientRegistry.
@@ -45,6 +44,74 @@ public class ClientRecord {
 
     }
 
+    public void updateRecord(JSONObject registryObject) {
+        String username = registryObject.get("username").toString();
+        String secret = registryObject.get("secret").toString();
+
+        // Update token num
+        Integer token = ((Long) registryObject.get("next_token_num")).intValue();
+        if (token > this.next_token_num) {
+            this.next_token_num = token;
+        }
+
+        // Update Tokens
+        JSONObject expectedTokensJson = (JSONObject) registryObject.get("expected_tokens");
+        ConcurrentHashMap<String, Integer> newExpectedTokens = toTokenHashMap(expectedTokensJson);
+        updateTokens(newExpectedTokens);
+
+        // Update sent messages
+        JSONObject sentMessagesJson = (JSONObject) registryObject.get("sent_messages");
+        ConcurrentHashMap<Integer, JSONObject> newSentMessages = toSentMessagesHashMap(sentMessagesJson);
+        updateSentMessages(newSentMessages);
+    }
+
+    public ConcurrentHashMap<Integer, JSONObject> toSentMessagesHashMap(JSONObject sentMessagesJson) {
+        ConcurrentHashMap<Integer, JSONObject> sentMessagesHashMap = new ConcurrentHashMap<Integer, JSONObject>();
+        sentMessagesJson.forEach((tokenObj, msgObj) -> {
+            Integer token = ((Long) tokenObj).intValue();
+            JSONObject msg = (JSONObject) msgObj;
+            sentMessagesHashMap.put(token, msg);
+        });
+        return sentMessagesHashMap;
+    }
+
+    private void updateSentMessages(ConcurrentHashMap<Integer, JSONObject> sentMessages) {
+        sentMessages.forEach((token, msg) -> {
+            if (!this.sent_messages.containsKey(token)) {
+                this.sent_messages.put(token, msg);
+            }
+        });
+    }
+
+
+    public ConcurrentHashMap<String, Integer> toTokenHashMap(JSONObject expectedTokensJson) {
+        ConcurrentHashMap<String, Integer> tokenHashMap = new ConcurrentHashMap<String, Integer>();
+        expectedTokensJson.forEach((userObj, tokenObj) -> {
+            String user = userObj.toString();
+            Integer token = ((Long) tokenObj).intValue();
+            tokenHashMap.put(user, token);
+        });
+        return tokenHashMap;
+    }
+
+    private void updateTokens(ConcurrentHashMap<String, Integer> newTokens) {
+        newTokens.forEach((user, token) -> {
+
+            // Update if it contains the user
+            if (this.expected_tokens.contains(user)) {
+                Integer currToken = this.expected_tokens.get(user);
+
+                // If the token provided is > the current recorded token, a msg has been sent, this is the ACK
+                if (token > currToken) {
+                    this.expected_tokens.put(user, token);
+                }
+            }
+            // Add new entry if entry is missing
+            else {
+                this.expected_tokens.put(user, token);
+            }
+        });
+    }
 
 
     // Update
@@ -70,6 +137,27 @@ public class ClientRecord {
     public void updateToken(String other_user) {
         Integer current_token = expected_tokens.get(other_user);
         expected_tokens.put(other_user, current_token + 1);
+    }
+
+    /**
+     * Checks whether or not the JSONObject representing a client record has the same secret as this ClientRecord
+     * @param clientRecord
+     * @return
+     */
+    public boolean sameSecret(JSONObject clientRecord) {
+        if (clientRecord.containsKey("secret")){
+            return this.secret.equals(clientRecord.get("secret").toString());
+        }
+        return false;
+    }
+
+    public JSONObject getMessage(Integer token) {
+        if (sent_messages.containsKey(token)) {
+            return sent_messages.get(token);
+        }
+        else {
+            return null;
+        }
     }
 
 }
--- activitystreamerserver/src/main/java/activitystreamer/server/ClientRegistry.java
+++ activitystreamerserver/src/main/java/activitystreamer/server/ClientRegistry.java
@@ -1,21 +1,24 @@
 package activitystreamer.server;
 
+import com.google.gson.Gson;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ClientRegistry {
 
-    private ArrayList<ClientRecord> clientRecords;
+    private ConcurrentHashMap<String, ClientRecord> clientRecords;
 
     // Client Records can either start empty, or they can be provided
     public ClientRegistry() {
-        this.clientRecords = new ArrayList<ClientRecord>();
+        this.clientRecords = new ConcurrentHashMap<String, ClientRecord>();
     }
 
-    public ClientRegistry(ArrayList<ClientRecord> providedClientRecords) {
+    public ClientRegistry(ConcurrentHashMap<String, ClientRecord> providedClientRecords) {
         this.clientRecords = providedClientRecords;
     }
 
@@ -28,19 +31,94 @@ public class ClientRegistry {
      * Assume:
      *  - Has a "command" field labeled "CLIENT_REGISTRY"
      *  - Has a "registry" field with a valid JSONArray (this is ensured anyway)
-     * @param msg
+     *  - TCP Ensures error-free data transfer, so we can assume all messages are well formed (as we created them)
+     * @param ...
      */
-    public void updateRecords(JSONObject msg) throws org.json.simple.parser.ParseException {
-        Object registryObject = msg.get("registry");
-        JSONArray registry;
-        if (registryObject instanceof JSONArray) {
-            registry = (JSONArray) registryObject;
-        }
-        else {
-            throw new ParseException(2);
-        }
+    public void updateRecords(JSONArray registry) throws org.json.simple.parser.ParseException {
+        // Iterate through Array
+        registry.forEach((clientRecordObject) -> {
 
+            // Convert each record into a JSONObject
+            JSONObject clientRecordJson = (JSONObject) clientRecordObject;
+            String username = clientRecordJson.get("username").toString();
 
+            // Update existing record
+            if (clientRecords.containsKey(username)) {
+                ClientRecord oldClientRecord = clientRecords.get(username);
+
+                // Update if the record has the correct secret
+                if (oldClientRecord.sameSecret(clientRecordJson)) {
+                    oldClientRecord.updateRecord(clientRecordJson);
+                }
+                // Conflicting ClientRecord username & secret combination. Conflict created during network partition.
+                // We delete both.
+                else {
+                    clientRecords.remove(username);
+                }
+            }
+            // Or create a new record
+            else {
+                clientRecords.put(username, new ClientRecord(clientRecordJson);
+            }
+        });
     }
 
+    /**
+     * Retrieve the server's Client Registry in the form of a JSONArray.
+     *
+     * Use GSON to convert ClientRecords to JSON
+     * https://github.com/google/gson/blob/master/UserGuide.md#TOC-Object-Examples
+     *
+     * @return JSONArray - containing the converted Client Registry
+     */
+    public JSONArray getRecordsJson() {
+
+        // Place ClientRecords into an array
+        ArrayList<ClientRecord> recordArray = new ArrayList<ClientRecord>();
+        clientRecords.forEach((username, record) -> {
+            recordArray.add(record);
+        });
+
+        // Convert ArrayList into JSONArray String
+        Gson gson = new Gson();
+        String jsonArrayString = gson.toJson(recordArray);
+        JSONArray recordJsonArray;
+
+        // Convert JSON compatible string into JSON
+        JSONParser parser = new JSONParser();
+        try {
+            recordJsonArray = (JSONArray) parser.parse(jsonArrayString);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+            recordJsonArray = new JSONArray();
+            recordJsonArray.add("failure");
+            return recordJsonArray;
+        }
+        return recordJsonArray;
+    }
+
+
+    public void addFreshClient(String username, String secret) {
+        clientRecords.put(username, new ClientRecord(username, secret));
+    }
+
+    /**
+     *
+     *
+     *
+     * @param username
+     * @param token
+     * @return Null if client or message not in registry. Otherwise returns the JSONObject message.
+     */
+    public JSONObject getMessage(String username, Integer token) {
+        if (clientRecords.containsKey(username)) {
+            ClientRecord record = clientRecords.get(username);
+            return record.getMessage(token);
+        }
+        else {
+            return null;
+        }
+
+    }
 }
